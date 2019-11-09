2822f6f176de47d5b845a78954aac3e0c61836d3
Thomas Welzel <thomaswelzel@MacBook-Pro.fritz.box>
2019-11-09 10:27:46 +0100

    test

--- services/RFID.js
+++ services/RFID.js
@@ -1,23 +1,43 @@
 exports.init = function(HID){
-  // Honeywell Scanner
-  var sys = require('sys');
-
-
-  var Hidstream = require('node-hid-stream').Hidstream;
-var d = new Hidstream({ vendorId: 5824, productId: 10203 });
-  console.log("yes init");
-
-  var d = new HID.HID(vid, pid);
-
-  d.on("data", function (data) {
-
-    console.log(data.toString('hex'));
-
-  });
-
-  d.on("error", function (error) {
-  console.log(error);
-  d.close();
-  });
+  / NodeJS includes
+var sys = require('sys');
+var fs = require('fs');
+// Stores the RFID id as it reconstructs from the stream.
+var id = '';
+// List of all RFID ids read
+var ids = [];
+// ARGUMENT 1:
+// Stream path, unique to your hardware.
+// List your available USB serial streams via terminal and choose one:
+//   ls /dev | grep usb
+// Had trouble with TTY, so used CU.
+// ARGUMENT 2:
+// Simplifies restruction of stream if one bit comes at a time.
+// However, I don't know if or how this setting affects performance.
+fs.createReadStream('/dev/usb/hiddev0', { bufferSize: 1 })
+.on('open', function(fd) {
+sys.puts('Begin scanning RFID tags.');
+})
+.on('end', function() {
+sys.puts('End of data stream.');
+})
+.on('close', function() {
+sys.puts('Closing stream.');
+})
+.on('error', function(error) {
+sys.debug(error);
+})
+.on('data', function(chunk) {
+	chunk = chunk.toString('ascii').match(/\w*/)[0]; // Only keep hex chars
+if ( chunk.length == 0 ) { // Found non-hex char
+if ( id.length > 0 ) { // The ID isn't blank
+ids.push(id); // Store the completely reconstructed ID
+sys.puts(id);
+		}
+		id = ''; // Prepare for the next ID read
+return;
+	}
+	id += chunk; // Concat hex chars to the forming ID
+});
 
 };
